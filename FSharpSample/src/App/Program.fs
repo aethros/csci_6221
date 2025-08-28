efe6fdd00936eae8efc67667394aed43626baa09
Stephen Hecht <stephenhecht@noreply>
2025-08-28 11:36:31 -0400

    init F# proj

---
+++ FSharpSample/src/App/Program.fs
@@ -0,0 +1,16 @@
+﻿// For more information see https://aka.ms/fsharp-console-apps
+
+open System
+open Library.Json
+
+[<EntryPoint>]
+let main args =
+    printfn "Hello from F#"
+
+    printfn "Nice command-line arguments! Here's what System.Text.Json has to say about them:"
+
+    let value, json = getJson {| args=args; year=System.DateTime.Now.Year |}
+    printfn $"Input: %0A{value}"
+    printfn $"Output: %s{json}"
+
+    0 // return an integer exit code
---
+++ FSharpSample/src/Library/Library.fs
@@ -0,0 +1,12 @@
+﻿namespace Library
+
+open System.Text.Json
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
+
+module Json =
+    let getJson value =
+        let json = JsonSerializer.Serialize(value)
+        value, json
