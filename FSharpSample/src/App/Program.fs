80462006d03179020b25f047deb31a32d4c26691
Stephen Hecht <stephenhecht@noreply>
2025-09-03 02:32:37 +0000

    update repo

--- FSharpSample/src/App/Program.fs
+++ FSharpSample/src/App/Program.fs
@@ -5,21 +5,20 @@ open Library.Json
 open Library.Say
 
 [<EntryPoint>]
-
 // let funcname (args: type): returnType = /* body */
 let main (args: array<string>): int =
     if args.Length < 3 then
         -1
 
     else 
-        hello args[1]
         // let var: type = value
+        let arg: string = args[1]
         let args: array<string> = args[2..];
+        let (value: {| args: array<string>; year: int |}), (json: string) = getJson {| args=args; year=DateTime.Now.Year |};
+
+        hello arg
         printfn "Hello from F#"
-
         printfn "Nice command-line arguments! Here's what System.Text.Json has to say about them:"
-
-        let (value: {| args: array<string>; year: int |}), (json: string) = getJson {| args=args; year=DateTime.Now.Year |}
         printfn $"Input: %0A{value}"
         printfn $"Output: %s{json}"
 
