6971ae8abc486c1c0c743f2e1d5836129538d953
Stephen Hecht <stephenhecht@noreply>
2025-09-21 17:48:23 +0000

    Isolated impure function

--- FSharpSample/src/App/Program.fs
+++ FSharpSample/src/App/Program.fs
@@ -4,22 +4,32 @@ open System
 open Library.Json
 open Library.Say
 
+// `type` typename = {| membername: type; |}
+type serialized = {| args: array<string>; year: int |}
+
+// Impure function
+let output (arg: string, value: serialized, json: string) : unit =
+    hello arg
+    printfn "Hello from F#"
+    printfn "Nice command-line arguments! Here's what System.Text.Json has to say about them:"
+    printfn $"Input: %0A{value}"
+    printfn $"Output: %s{json}"
+
 [<EntryPoint>]
 // let funcname (args: type): returnType = /* body */
-let main (args: array<string>): int =
+let main (args: array<string>) : int =
     if args.Length < 3 then
         -1
-
-    else 
+    else
         // let var: type = value
         let arg: string = args[1]
-        let args: array<string> = args[2..];
-        let (value: {| args: array<string>; year: int |}), (json: string) = getJson {| args=args; year=DateTime.Now.Year |};
+        let args: array<string> = args[2..]
 
-        hello arg
-        printfn "Hello from F#"
-        printfn "Nice command-line arguments! Here's what System.Text.Json has to say about them:"
-        printfn $"Input: %0A{value}"
-        printfn $"Output: %s{json}"
+        let (value: serialized), (json: string) =
+            getJson
+                {| args = args
+                   year = DateTime.Now.Year |}
+
+        output (arg, value, json) // Side effects
 
         0 // return an integer exit code
--- FSharpSample/src/Library/Library.fs
+++ FSharpSample/src/Library/Library.fs
@@ -3,10 +3,9 @@
 open System.Text.Json
 
 module Say =
-    let hello (name: string): unit =
-        printfn "Hello %s" name
+    let hello (name: string) : unit = printfn "Hello %s" name
 
 module Json =
-    let getJson<'T> (value: 'T): 'T * string =
+    let getJson<'T> (value: 'T) : 'T * string =
         let json: string = JsonSerializer.Serialize value
         value, json
